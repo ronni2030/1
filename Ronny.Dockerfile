08168fea9f6398931e67805732bafda1b3bf41dc
ronni2030 <rav.villa@yavirac.edu.ec>
2025-11-18 22:14:32 -0500

    dede

--- Ronny.Dockerfile
+++ Ronny.Dockerfile
@@ -8,4 +8,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["python", "app.Core.py"]
+CMD ["python", "app_Core.py"]
+
--- test_suma.py
+++ test_suma.py
@@ -1,8 +1,9 @@
-from app.Core import app
+from app_Core import app
 
 def test_suma_endpoint():
     client = app.test_client()
     response = client.get("/suma?a=3&b=5")
     data = response.get_json()
+
     assert response.status_code == 200
     assert data["resultado"] == 8
